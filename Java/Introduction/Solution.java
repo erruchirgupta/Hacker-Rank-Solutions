0e0b95865a28dc429e58ab56ccb92780b572cf86
Ruchir Gupta <erruchirgupta@gmail.com>
2018-06-27 23:46:15 +0530

    Java Stdin and Stdout I

---
+++ Java/Introduction/Solution.java
@@ -0,0 +1,23 @@
+//     Author: Ruchir Gupta
+//     Github: https://github.com/erruchirgupta
+// HackerRank: https://hackerrank.com/RuchirGupta
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int ar[] = new int[3];
+         
+        for(int i=0; i < ar.length; i++){
+            ar[i] = scan.nextInt();
+        }
+        
+        for(int i=0; i < ar.length; i++){
+            System.out.println(ar[i]);
+        }
+        
+    }
+}
