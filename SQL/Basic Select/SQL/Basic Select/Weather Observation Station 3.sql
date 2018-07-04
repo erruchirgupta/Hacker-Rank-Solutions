4f0f21e01e3fef79e7269efe8e55c90b1124951b
Ruchir Gupta <Ruchir.Gupta.ap@nielsen.com>
2018-07-04 15:48:19 +0530

    Revert "Weather Observation Station 3.sql"
    This reverts commit 5f9e915cd04a77a86557e4dac7a6d61855e62e51.

--- SQL/Basic Select/SQL/Basic Select/Weather Observation Station 3.sql
+++
@@ -1,5 +0,0 @@
---     Author: Ruchir Gupta
---     Github: https://github.com/erruchirgupta
--- HackerRank: https://hackerrank.com/RuchirGupta
-
-select DISTINCT CITY FROM STATION where MOD(ID,2)=0;
