92c4b7779095848921d06f55a38621f168270ff8
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-04 15:42:32 +0530

    Weather Observation Station 3.sql

---
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,5 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) AS DIFF FROM STATION;
