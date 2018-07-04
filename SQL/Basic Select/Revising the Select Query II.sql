66cbf26b99346a62a24a5b7201c1b56cc126229d
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-05 01:43:39 +0530

    -- ORACLE SQL SOLUTIONS

---
+++ SQL/Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,7 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+-- ORACLE SQL SOLUTION
+
+select NAME from CITY where POPULATION > 120000 and COUNTRYCODE = 'USA';
