5f9e915cd04a77a86557e4dac7a6d61855e62e51
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-04 15:44:44 +0530

    Weather Observation Station 3.sql

---
+++ SQL/Basic Select/SQL/Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,5 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+select DISTINCT CITY FROM STATION where MOD(ID,2)=0;
