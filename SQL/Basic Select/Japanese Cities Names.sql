a637f9f65e72a0761fcadbd0749efae1565be62c
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-05 01:48:14 +0530

    -- ORACLE SQL SOLUTIONS

---
+++ SQL/Basic Select/Japanese Cities Names.sql
@@ -0,0 +1,7 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+-- ORACLE SQL SOLUTION
+
+SELECT NAME FROM CITY where COUNTRYCODE = 'JPN';
