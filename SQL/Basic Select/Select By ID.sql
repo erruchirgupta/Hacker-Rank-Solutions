6cc23ab8e0c7f3aebd50647a027d0e77fdd230ee
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-05 01:45:55 +0530

    -- ORACLE SQL SOLUTIONS

---
+++ SQL/Basic Select/Select By ID.sql
@@ -0,0 +1,7 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+-- ORACLE SQL SOLUTION
+
+SELECT * FROM CITY where ID = 1661;
