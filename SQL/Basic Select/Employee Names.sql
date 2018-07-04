f946676bb514c60f95890422443e7cfc07b32926
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-05 01:52:11 +0530

    -- ORACLE SQL SOLUTIONS

---
+++ SQL/Basic Select/Employee Names.sql
@@ -0,0 +1,7 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+-- ORACLE SQL SOLUTION
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME;
