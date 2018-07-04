5a5e7177cdcba37cfcc06e994cf4139b47d8677a
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-05 01:35:43 +0530

    Create Weather Observation Station 12.sql

---
+++ SQL/Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,7 @@
+--     Author: Ruchir Gupta
+--     Github: https://github.com/erruchirgupta
+-- HackerRank: https://hackerrank.com/RuchirGupta
+
+SELECT DISTINCT CITY FROM STATION
+WHERE NOT REGEXP_LIKE (CITY, '^[aeiouAEIOU]|[aeiouAEIOU]$') 
+ORDER BY CITY;
