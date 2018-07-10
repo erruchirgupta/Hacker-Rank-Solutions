bc29f52d42cf3175ce58b1b4b9db8e4c85c28a01
Ruchir Gupta <erruchirgupta@gmail.com>
2018-07-10 11:07:09 +0530

    Create Solution.java

---
+++ Algorithms/Warmup/Plus Minus/Solution.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        float p=0,n=0,l=arr.length;
+        for(int x : arr){
+            if(x>0) p++;
+            if(x<0) n++;            
+        }
+        System.out.println(p/l);
+        System.out.println(n/l);
+        System.out.println((l-p-n)/l);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
